a737f3b8974c050b1b819803e286abb48a5731b2
iahmed <ahmadsameh1@proton.me>
2024-04-17 21:29:47 +0200

    Adding Country Business and DataAccess layers.

---
+++ Project/DVLD_DataAccess/clsCountryData.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public class clsCountryData
+    {
+        public static bool GetCountryInfoByID(int ID, ref string CountryName)
+        {
+            bool isFound = false;
+            SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "select * from Countries where CountryID = @CountryID;";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CountryID", ID);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    CountryName = (string)reader["CountryName"];
+                }
+                else
+                    isFound = false;
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally { conn.Close(); }
+
+            return isFound;
+        }
+
+        public static bool GetCountryInfoByName(string CountryName, ref int ID)
+        {
+            bool isFound = false;
+            SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "select * from Countries where CountryName = @CountryName;";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@CountryName", CountryName);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+                    ID = (int)reader["CountryID"];
+                }
+                else
+                    isFound = false;
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally { conn.Close(); }
+
+            return isFound;
+        }
+    
+        public static DataTable GetAllCountries()
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "select * from Countries order by CountryName;";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            try
+            {
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+            }
+            finally { conn.Close(); }
+
+            return dt;
+        }
+    
+    }
+}
